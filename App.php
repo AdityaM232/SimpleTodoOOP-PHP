649be6b17efc80720ea126d32038d99e8c8b5f5b
AdityaManggala <aditya.manggala12345@gmail.com>
2022-05-06 00:52:04 +0700

    ubah penyimpanan dari memory ke DB

--- App.php
+++ App.php
@@ -4,12 +4,15 @@ require_once __DIR__ . "/Helper/InputHelper.php";
 require_once __DIR__ . "/Repository/TodolistRepository.php";
 require_once __DIR__ . "/Service/TodolistService.php";
 require_once __DIR__ . "/View/TodolistView.php";
+require_once __DIR__ . "/Config/Database.php";
 
+use Config\Database;
 use Repository\TodolistRepositoryImpl;
 use Service\TodolistServiceImpl;
 use View\TodolistView;
+$connection = Database::getConnection();
 
-$todolistRepo = new TodolistRepositoryImpl();
+$todolistRepo = new TodolistRepositoryImpl($connection);
 $todolistService = new TodolistServiceImpl($todolistRepo);
 $todolistView = new TodolistView($todolistService);
 
